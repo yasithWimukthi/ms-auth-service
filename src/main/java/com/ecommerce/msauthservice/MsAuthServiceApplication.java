d65736237fec8b904aac9138390f2c230ef0f51f
yasithwimukthi <wimukthiyasith@gmail.com>
2025-08-20 11:31:20 +0530

    Initial commit

---
+++ src/main/java/com/ecommerce/msauthservice/MsAuthServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.msauthservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsAuthServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsAuthServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ecommerce/msauthservice/MsAuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.msauthservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsAuthServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
